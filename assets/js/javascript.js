a7b8da966c09ffacde8a61d1defebdc13d986177
ih-sonnet <sonat1001@outlook.com>
2020-08-18 02:36:35 +0600

    user profile added

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -28,21 +28,63 @@ button.onclick = function() {
 
 
 //slide
+//
+// var slideIndex = 1;
+// showDivs(slideIndex);
+//
+// function plusDivs(n) {
+// showDivs(slideIndex += n);
+// }
+//
+// function showDivs(n) {
+// var i;
+// var x = document.getElementsByClassName("mySlides");
+// if (n > x.length) {slideIndex = 1}
+// if (n < 1) {slideIndex = x.length}
+// for (i = 0; i < x.length; i++) {
+//   x[i].style.display = "none";
+// }
+// x[slideIndex-1].style.display = "block";
+// }
+//
 
-var slideIndex = 1;
-showDivs(slideIndex);
+//AutoSlide
 
-function plusDivs(n) {
-showDivs(slideIndex += n);
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 4000); // Change image every 2 seconds
 }
 
-function showDivs(n) {
-var i;
-var x = document.getElementsByClassName("mySlides");
-if (n > x.length) {slideIndex = 1}
-if (n < 1) {slideIndex = x.length}
-for (i = 0; i < x.length; i++) {
-  x[i].style.display = "none";
+//navbar
+
+function openNavleft() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
 }
-x[slideIndex-1].style.display = "block";
+
+function closeNavleft() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+
+//navbar right
+
+function openNavright() {
+  document.getElementById("mySidebar2").style.width = "250px";
+  document.getElementById("main").style.marginRight = "250px";
+}
+
+function closeNavright() {
+  document.getElementById("mySidebar2").style.width = "0";
+  document.getElementById("main").style.marginRight= "0";
 }
